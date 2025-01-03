5b09ef8c3077c5302b38fb1a89e377be83faaea2
Alina Grin <70023302+chebupelkaa@users.noreply.github.com>
2025-01-03 03:40:41 +0300

    first commit

---
+++ src/TodoItem.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+
+export function TodoItem({completed,id,title,toggleTodo,deleteTodo}){
+
+    return (
+        <li>
+            <label>
+                <input type="checkbox" checked={completed} 
+                onChange={e=>toggleTodo(id,e.target.checked)}
+                />
+                 <span className={completed ? "completed" : ""}>{title}</span>
+            </label>
+            <button 
+            onClick={()=>deleteTodo(id)} 
+            className="btn btn-danger">Delete</button>
+        </li>
+    )
+}
+
